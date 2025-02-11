b701867d7de4e08627e41d9681ee873d4344e20f
Jorge Vargas <jorgev259@outlook.es>
2025-02-10 23:10:38 -0600

    Refresh after logout

--- src/components/header/LogoutButton.tsx
+++ src/components/header/LogoutButton.tsx
@@ -3,8 +3,13 @@ import Button from 'components/Button'
 import { signOut } from 'utils/auth-client'
 
 export default function LogoutBtn() {
+  async function handleClick() {
+    await signOut()
+    location.reload()
+  }
+
   return (
-    <Button className='rounded-t-none' onClick={() => signOut()}>
+    <Button className='rounded-t-none' onClick={handleClick}>
       {m.logout()}
     </Button>
   )
