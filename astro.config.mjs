c80cc79d16773dfbceb196ab6d5c428956658803
Jorge Vargas <jorgev259@outlook.es>
2025-04-26 22:38:10 -0600

    Implement /game/list

--- astro.config.mjs
+++ astro.config.mjs
@@ -52,7 +52,6 @@ export default defineConfig({
     '/anim/[id]': { status: 307, destination: '/maintenance' },
     '/anim/list': { status: 307, destination: '/maintenance' },
     '/game/[slug]': { status: 307, destination: '/maintenance' },
-    '/game/list': { status: 307, destination: '/maintenance' },
     '/platform/list': { status: 307, destination: '/maintenance' },
     '/platform/[id]': { status: 307, destination: '/maintenance' },
     '/profile': { status: 307, destination: '/maintenance' },
