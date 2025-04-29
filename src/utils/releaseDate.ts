af3cede12541b93d5bf7c2d137345a17fc911d0b
Jorge Vargas <jorgev259@outlook.es>
2025-04-28 18:06:34 -0600

    Fix ReleaseDate component

--- src/utils/releaseDate.ts
+++
@@ -1,6 +0,0 @@
-const locale =
-  navigator && navigator.languages && navigator.languages.length ? navigator.languages[0] : navigator.language
-
-const releaseDate = (date: Date) => new Intl.DateTimeFormat(locale, { dateStyle: 'medium' }).format(date)
-
-export default releaseDate
