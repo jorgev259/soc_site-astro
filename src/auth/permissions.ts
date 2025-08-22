9414c01d3bf5935f0291c18d07644a559cbcbaea
Jorge Vargas <jorgev259@outlook.es>
2025-08-22 12:04:51 -0600

    Fix admin permissions

--- src/auth/permissions.ts
+++ src/auth/permissions.ts
@@ -1,12 +1,12 @@
 import { createAccessControl } from 'better-auth/plugins/access'
-import { defaultStatements } from 'better-auth/plugins/admin/access'
+import { adminAc, defaultStatements } from 'better-auth/plugins/admin/access'
 
-const statement = {
-  ...defaultStatements,
-  site: ['skip_ads'],
-  cms: ['publish', 'update'],
-  request: ['create']
-} as const
+const customStatements = { site: ['skip_ads'], cms: ['publish', 'update'], request: ['create'] }
+const statement = { ...defaultStatements, ...customStatements } as const
 export const ac = createAccessControl(statement)
 
-export const roles = { donator: ac.newRole({ site: ['skip_ads'] }), mod: ac.newRole({ cms: ['update'] }) } as const
+export const roles = {
+  admin: ac.newRole({ ...adminAc.statements, ...customStatements }),
+  donator: ac.newRole({ site: ['skip_ads'] }),
+  mod: ac.newRole({ cms: ['update'] })
+} as const
